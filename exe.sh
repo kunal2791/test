3e1bd35ef139016a39d431e7ec284fe57ed58ffe
Kunal Jha <kunal2791@gmail.com>
2020-05-06 21:15:33 +0530

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -2,4 +2,4 @@ pub_ip=`curl http://checkip.amazonaws.com`
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update -y
 sudo apt-get install ansible -y
-ansible-playbook -i inventory openvpn.yml --extra-vars "server=$pub_ip"
+ansible-playbook -i inventory openvpn.yml
